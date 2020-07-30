d9b923c12458eabf432e1d1725ad42f7da94fc2d
Danilo <dbb1307@gmail.com>
2020-07-30 19:46:34 -0300

    logout

--- src/app/pages/empresas/empresas.page.ts
+++ src/app/pages/empresas/empresas.page.ts
@@ -1,3 +1,5 @@
+import { NavController, ToastController } from '@ionic/angular';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empresas.page.scss'],
 })
 export class EmpresasPage implements OnInit {
+  datastorage: any;
+  nome: string;
 
-  constructor() { }
+  constructor(
+    private storage: NativeStorage,
+    public navCtrl: NavController,
+    private toastCtrl: ToastController
+    ) { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter(){
+    this.storage.getItem('storage_xxx').then((res)=>{
+      console.log(res);
+      this.datastorage = res;
+      this.nome = this.datastorage.seu_nome;
+    });
+  }
+
+
+
+  async logout(){
+    this.storage.clear();
+    this.navCtrl.navigateRoot(['/inicio']);
+    const toast = await this.toastCtrl.create({
+      message: 'Logout realizado com sucesso!',
+      duration: 1500
+    });
+    toast.present();
   }
 
 }
